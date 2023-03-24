c30d04d0f08321d79535f19a463d9fbbd3a3c5d1
moton03 <kirari.moton@outlook.com>
2023-03-24 20:48:25 +0900

    Update

--- src/GREditorialSupport/GREditorialSupport/Form1.Designer.cs
+++ src/GREditorialSupport/GREditorialSupport/Form1.Designer.cs
@@ -30,92 +30,94 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             mainPanel = new FlowLayoutPanel();
+            editingMethodomboBox = new ComboBox();
             editFlowLayoutPanel = new FlowLayoutPanel();
-            contentEditCheckBox = new CheckBox();
-            startButton = new Button();
-            postEditCheckBox = new CheckBox();
-            editLabel = new Label();
             mainEditCheckBox = new CheckBox();
+            contentEditCheckBox = new CheckBox();
+            postEditCheckBox = new CheckBox();
+            startButton = new Button();
             mainPanel.SuspendLayout();
             editFlowLayoutPanel.SuspendLayout();
             SuspendLayout();
             // 
             // mainPanel
             // 
+            mainPanel.Controls.Add(editingMethodomboBox);
             mainPanel.Controls.Add(editFlowLayoutPanel);
             mainPanel.Controls.Add(startButton);
             mainPanel.Dock = DockStyle.Fill;
             mainPanel.FlowDirection = FlowDirection.TopDown;
             mainPanel.Location = new Point(0, 0);
             mainPanel.Name = "mainPanel";
-            mainPanel.Size = new Size(800, 450);
+            mainPanel.Size = new Size(332, 200);
             mainPanel.TabIndex = 0;
+            // 
+            // editingMethodomboBox
+            // 
+            editingMethodomboBox.FormattingEnabled = true;
+            editingMethodomboBox.Items.AddRange(new object[] { "GitHubDevWebEditor", "VisualStudioCode", "VisualStudioCodeInsiders" });
+            editingMethodomboBox.Location = new Point(3, 3);
+            editingMethodomboBox.Name = "editingMethodomboBox";
+            editingMethodomboBox.Size = new Size(240, 33);
+            editingMethodomboBox.TabIndex = 0;
+            editingMethodomboBox.Text = "GitHubDevWebEditor";
             // 
             // editFlowLayoutPanel
             // 
-            editFlowLayoutPanel.Controls.Add(editLabel);
             editFlowLayoutPanel.Controls.Add(mainEditCheckBox);
             editFlowLayoutPanel.Controls.Add(contentEditCheckBox);
             editFlowLayoutPanel.Controls.Add(postEditCheckBox);
-            editFlowLayoutPanel.Dock = DockStyle.Fill;
-            editFlowLayoutPanel.Location = new Point(3, 3);
+            editFlowLayoutPanel.FlowDirection = FlowDirection.TopDown;
+            editFlowLayoutPanel.Location = new Point(3, 42);
             editFlowLayoutPanel.Name = "editFlowLayoutPanel";
-            editFlowLayoutPanel.Size = new Size(112, 150);
-            editFlowLayoutPanel.TabIndex = 2;
-            // 
-            // contentEditCheckBox
-            // 
-            contentEditCheckBox.AutoSize = true;
-            contentEditCheckBox.Location = new Point(3, 63);
-            contentEditCheckBox.Name = "contentEditCheckBox";
-            contentEditCheckBox.Size = new Size(101, 29);
-            contentEditCheckBox.TabIndex = 0;
-            contentEditCheckBox.Text = "Content";
-            contentEditCheckBox.UseVisualStyleBackColor = true;
-            // 
-            // startButton
-            // 
-            startButton.Location = new Point(3, 159);
-            startButton.Name = "startButton";
-            startButton.Size = new Size(112, 34);
-            startButton.TabIndex = 1;
-            startButton.Text = "Start";
-            startButton.UseVisualStyleBackColor = true;
-            // 
-            // postEditCheckBox
-            // 
-            postEditCheckBox.AutoSize = true;
-            postEditCheckBox.Location = new Point(3, 98);
-            postEditCheckBox.Name = "postEditCheckBox";
-            postEditCheckBox.Size = new Size(73, 29);
-            postEditCheckBox.TabIndex = 1;
-            postEditCheckBox.Text = "Post";
-            postEditCheckBox.UseVisualStyleBackColor = true;
-            // 
-            // editLabel
-            // 
-            editLabel.AutoSize = true;
-            editLabel.Location = new Point(3, 0);
-            editLabel.Name = "editLabel";
-            editLabel.Size = new Size(42, 25);
-            editLabel.TabIndex = 2;
-            editLabel.Text = "Edit";
+            editFlowLayoutPanel.Size = new Size(286, 106);
+            editFlowLayoutPanel.TabIndex = 0;
             // 
             // mainEditCheckBox
             // 
             mainEditCheckBox.AutoSize = true;
-            mainEditCheckBox.Location = new Point(3, 28);
+            mainEditCheckBox.Location = new Point(3, 3);
             mainEditCheckBox.Name = "mainEditCheckBox";
             mainEditCheckBox.Size = new Size(77, 29);
             mainEditCheckBox.TabIndex = 3;
             mainEditCheckBox.Text = "Main";
             mainEditCheckBox.UseVisualStyleBackColor = true;
             // 
+            // contentEditCheckBox
+            // 
+            contentEditCheckBox.AutoSize = true;
+            contentEditCheckBox.Location = new Point(3, 38);
+            contentEditCheckBox.Name = "contentEditCheckBox";
+            contentEditCheckBox.Size = new Size(101, 29);
+            contentEditCheckBox.TabIndex = 0;
+            contentEditCheckBox.Text = "Content";
+            contentEditCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // postEditCheckBox
+            // 
+            postEditCheckBox.AutoSize = true;
+            postEditCheckBox.Location = new Point(3, 73);
+            postEditCheckBox.Name = "postEditCheckBox";
+            postEditCheckBox.Size = new Size(73, 29);
+            postEditCheckBox.TabIndex = 1;
+            postEditCheckBox.Text = "Post";
+            postEditCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            startButton.Location = new Point(3, 154);
+            startButton.Name = "startButton";
+            startButton.Size = new Size(112, 34);
+            startButton.TabIndex = 1;
+            startButton.Text = "Start";
+            startButton.UseVisualStyleBackColor = true;
+            startButton.Click += startButton_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(332, 200);
             Controls.Add(mainPanel);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
@@ -129,11 +131,15 @@
         #endregion
 
         private FlowLayoutPanel mainPanel;
-        private FlowLayoutPanel editFlowLayoutPanel;
         private CheckBox contentEditCheckBox;
         private Button startButton;
-        private Label editLabel;
         private CheckBox mainEditCheckBox;
         private CheckBox postEditCheckBox;
+        private TabControl optionTabControl;
+        private TabPage editTabPage;
+        private FlowLayoutPanel editFlowLayoutPanel;
+        private TabPage editingMethodTabPage;
+        private FlowLayoutPanel tabEditingMethodFlowLayoutPanel;
+        private ComboBox editingMethodomboBox;
     }
 }
--- src/GREditorialSupport/GREditorialSupport/Form1.cs
+++ src/GREditorialSupport/GREditorialSupport/Form1.cs
@@ -5,6 +5,72 @@ namespace GREditorialSupport
         public Form1()
         {
             InitializeComponent();
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            switch (editingMethodomboBox.Text)
+            {
+                case "GitHubDevWebEditor":
+                    if (mainEditCheckBox.Checked)
+                    {
+                        var startInfo = new System.Diagnostics.ProcessStartInfo("https://github.dev/Game-Resources/Game-Resources.github.io");
+                        startInfo.UseShellExecute = true;
+                        System.Diagnostics.Process.Start(startInfo);
+                    }
+                    if (contentEditCheckBox.Checked)
+                    {
+                        var startInfo = new System.Diagnostics.ProcessStartInfo("https://github.dev/Game-Resources/Content");
+                        startInfo.UseShellExecute = true;
+                        System.Diagnostics.Process.Start(startInfo);
+                    }
+                    if (postEditCheckBox.Checked)
+                    {
+                        var startInfo = new System.Diagnostics.ProcessStartInfo("https://github.dev/Game-Resources/Post");
+                        startInfo.UseShellExecute = true;
+                        System.Diagnostics.Process.Start(startInfo);
+                    }
+
+                    break;
+                case "VisualStudioCode":
+                    if (mainEditCheckBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("cmd.exe", "/k github \"C:\\GitHub\\Game-Resources.github.io\"");
+                        System.Diagnostics.Process.Start("cmd.exe", "/k code \"C:\\GitHub\\Game-Resources.github.io\"");
+                    }
+                    if (contentEditCheckBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("cmd.exe", "/k github \"C:\\GitHub\\Content\"");
+                        System.Diagnostics.Process.Start("cmd.exe", "/k code \"C:\\GitHub\\Content\"");
+                    }
+                    if (postEditCheckBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("cmd.exe", "/k github \"C:\\GitHub\\Post\"");
+                        System.Diagnostics.Process.Start("cmd.exe", "/k code \"C:\\GitHub\\Post\"");
+                    }
+
+                    break;
+                case "VisualStudioCodeInsiders":
+                    if (mainEditCheckBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("cmd.exe", "/k github \"C:\\GitHub\\Game-Resources.github.io\"");
+                        System.Diagnostics.Process.Start("cmd.exe", "/k code-insiders \"C:\\GitHub\\Game-Resources.github.io\"");
+                    }
+                    if (contentEditCheckBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("cmd.exe", "/k github \"C:\\GitHub\\Content\"");
+                        System.Diagnostics.Process.Start("cmd.exe", "/k code-insiders \"C:\\GitHub\\Content\"");
+                    }
+                    if (postEditCheckBox.Checked)
+                    {
+                        System.Diagnostics.Process.Start("cmd.exe", "/k github \"C:\\GitHub\\Post\"");
+                        System.Diagnostics.Process.Start("cmd.exe", "/k code-insiders \"C:\\GitHub\\Post\"");
+                    }
+
+                    break;
+            }
         }
     }
 }
